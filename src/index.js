2e982cf14be5df55d0af9271eb42b91d67fc5bd4
Carlos Valencia <rasfreed@gmail.com>
2022-08-20 14:10:47 -0500

    small changes in the structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-let mainWindow;
-
 function createWindow () {
-  mainWindow = new BrowserWindow({
+  let mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
-  })
-
+  });
   mainWindow.loadFile('index.html')
 }
 
